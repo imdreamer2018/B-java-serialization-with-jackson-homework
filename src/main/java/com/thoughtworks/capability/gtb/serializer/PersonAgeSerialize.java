6984b5753eef994271a8526f3c1c51cbb8df4615
yangqian <743295483@qq.com>
2020-10-20 17:30:09 +0800

    [yangqian] fix: using PersonAgeSerialize and JsonInclude

---
+++ src/main/java/com/thoughtworks/capability/gtb/serializer/PersonAgeSerialize.java
@@ -0,0 +1,15 @@
+package com.thoughtworks.capability.gtb.serializer;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+
+import java.io.IOException;
+
+public class PersonAgeSerialize extends JsonSerializer<Integer> {
+
+    @Override
+    public void serialize(Integer id, JsonGenerator gen, SerializerProvider serializers) throws IOException {
+        gen.writeNumber(0);
+    }
+}
